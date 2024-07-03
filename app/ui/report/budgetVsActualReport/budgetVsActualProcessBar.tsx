bc3fca9593f570942875891311fa97a01e0f59a9
chauthutran <cthutran@gmail.com>
2024-07-03 14:53:46 +0900

    Update budgetVsActualProcessBar.tsx

--- app/ui/report/budgetVsActualReport/budgetVsActualProcessBar.tsx
+++ app/ui/report/budgetVsActualReport/budgetVsActualProcessBar.tsx
@@ -13,17 +13,30 @@ import {
 import * as ReportService from "@/lib/services/reportService";
 
 type ProgressBarProps = {
+	name: string;
 	percentage: number;
 };
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ percentage }) => {
+const ProgressBar: React.FC<ProgressBarProps> = ({ name, percentage }) => {
+
+	const bgColor = (percentage > 100) ? "bg-red-500" : "bg-blue-500";
+	const textColor = (percentage > 100) ? "text-red-500" : "text-blue-500";
+
 	return (
-		<div className="w-full bg-gray-200 rounded-full h-4 overflow-hidden">
-			<div
-				className="bg-blue-500 h-full"
-				style={{ width: `${percentage}%` }}
-			></div>
+		<div className="mb-4">
+			<h2 className="font-semibold">{name}</h2>
+			<div className="w-full bg-gray-200 rounded-full h-4 overflow-hidden">
+				<div
+					className={`${bgColor} h-full`}
+					style={{ width: `${percentage}%` }}
+				></div>
+			</div>
+			<p className={`text-sm mt-2 ${textColor}`}>
+				{percentage.toFixed(2)}% of the budget used
+			</p>
 		</div>
+
+
 	);
 };
 
@@ -39,13 +52,7 @@ export default function BudgetVsActualProcessBar({ data }: { data: JSONObject })
 	return (
 		<div className="mb-10">
 			{reportData.map((item) => (
-				<div key={item.name} className="mb-4">
-					<h2 className="font-semibold">{item._id.categoryName}</h2>
-					<ProgressBar percentage={calculatePercentage(item.actualAmount, item.budgetAmount)} />
-					<p className="text-sm mt-2">
-						{calculatePercentage(item.expenseAmount, item.budgetAmount).toFixed(2)}% of the budget used
-					</p>
-				</div>
+				<ProgressBar percentage={calculatePercentage(item.expenseAmount, item.budgetAmount)} name={item._id.categoryName} />
 			))}
 		</div>
 	);
